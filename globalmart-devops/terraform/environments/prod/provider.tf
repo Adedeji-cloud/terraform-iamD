d1c27e44ea895a3348facad8a4c09bdfa35a5387
Adedeji Adesanya <adesanyaadedeji63@gmail.com>
2026-07-19 15:09:12 +0100

    Migrate to S3 remote state backend with DynamoDB locking

--- globalmart-devops/terraform/environments/prod/provider.tf
+++ globalmart-devops/terraform/environments/prod/provider.tf
@@ -1,3 +1,13 @@
+terraform {
+  backend "s3" {
+    bucket         = "globmart-terraform-state-142280718160"
+    key            = "prod/terraform.tfstate"
+    region         = "eu-west-1"
+    dynamodb_table = "globmart-terraform-locks"
+    encrypt        = true
+  }
+}
+
 provider "aws" {
   region = var.aws_region
 }
